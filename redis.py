4bcfd63a96b675a93eead98620b1818f44d26761
Blank <blank@idata.cn>
2021-12-10 12:20:46 +0800

    add redis

---
+++ redis.py
@@ -0,0 +1,16 @@
+import aioredis  # pip install aioredis==2
+
+
+def get_asyn_redis(host=None, port=None, password=None, db:int=None, max_connections=10):
+    """ 获取异步reids连接 """
+    kwargs = dict(
+        port=port,
+        db=db,
+        max_connections=max_connections,
+        encoding="utf-8",
+        decode_responses=True
+    )
+    if password:
+        kwargs['password'] = password
+    pool = aioredis.ConnectionPool.from_url(f"redis://{host}",**kwargs)
+    return aioredis.Redis(connection_pool=pool)
