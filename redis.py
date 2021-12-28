0c8bb1617e16974c8c8d1960485d0dc14829a12a
blank <8783833+blank-lin@user.noreply.gitee.com>
2021-12-28 16:35:18 +0800

    updata redis 2.0

--- redis.py
+++ redis.py
@@ -1,9 +1,22 @@
-import aioredis  # pip install aioredis==2
+#!/usr/bin/env python3
+# -*- encoding: utf-8 -*-
+'''
+@File   :   redis.py
+@Time   :   2021/12/27 17:32
+@Author :   Blank
+@Version:   2.0
+@Desc   :   Redis pcblib
+'''
+
+from typing import Any, Dict, Union
+
+import aioredis
+from aioredis.connection import ConnectionPool  # pip install aioredis==2
 
 
-def get_asyn_redis(host=None, port=None, password=None, db:int=None, max_connections=10):
-    """ 获取异步reids连接 """
-    kwargs = dict(
+def get_asyn_redis(host: str = '127.0.0.1', port: int = 6379, password: str = None, db: Union[str, int] = 0, max_connections: int = 32, **kwargs):
+    """ 使用连接池创建reids客户端 """
+    conf = dict(
         port=port,
         db=db,
         max_connections=max_connections,
@@ -11,6 +24,158 @@ def get_asyn_redis(host=None, port=None, password=None, db:int=None, max_connect
         decode_responses=True
     )
     if password:
-        kwargs['password'] = password
-    pool = aioredis.ConnectionPool.from_url(f"redis://{host}",**kwargs)
-    return aioredis.Redis(connection_pool=pool)
+        conf['password'] = password
+    pool = aioredis.ConnectionPool.from_url(f"redis://{host}",**conf)
+    return AsyncRedisDB(connection_pool=pool, **kwargs)
+
+class AsyncRedisDB(aioredis.Redis):
+
+    def __init__(self, *, host: str = "localhost", port: int = 6379, db: Union[str, int] = 0, password: str = None, socket_timeout: float = None, socket_connect_timeout: float = None, socket_keepalive: bool = None, socket_keepalive_options: Dict[str, Any] = None, connection_pool: ConnectionPool = None, unix_socket_path: str = None, encoding: str = "utf-8", encoding_errors: str = "strict", decode_responses: bool = False, retry_on_timeout: bool = False, ssl: bool = False, ssl_keyfile: str = None, ssl_certfile: str = None, ssl_cert_reqs: str = "required", ssl_ca_certs: str = None, ssl_check_hostname: bool = False, max_connections: int = None, single_connection_client: bool = False, health_check_interval: int = 0, client_name: str = None, username: str = None):
+        super().__init__(host=host, port=port, db=db, password=password, socket_timeout=socket_timeout, socket_connect_timeout=socket_connect_timeout, socket_keepalive=socket_keepalive, socket_keepalive_options=socket_keepalive_options, connection_pool=connection_pool, unix_socket_path=unix_socket_path, encoding=encoding, encoding_errors=encoding_errors, decode_responses=decode_responses, retry_on_timeout=retry_on_timeout, ssl=ssl, ssl_keyfile=ssl_keyfile, ssl_certfile=ssl_certfile, ssl_cert_reqs=ssl_cert_reqs, ssl_ca_certs=ssl_ca_certs, ssl_check_hostname=ssl_check_hostname, max_connections=max_connections, single_connection_client=single_connection_client, health_check_interval=health_check_interval, client_name=client_name, username=username)
+
+        self._load_lua_script()
+
+    def _load_lua_script(self):
+        """ 加载 lua 脚本 """
+        self._zset_set_score_script = self.register_script("""
+            local key = KEYS[1]
+            local min_score = ARGV[1]
+            local max_score = ARGV[2]
+            local set_score = ARGV[3]
+            local num = ARGV[4]
+
+            -- get zset data
+            local datas = nil
+            if num ~= '' then
+                datas = redis.call('zrangebyscore', key, min_score, max_score, 'withscores', 'limit', 0, num)
+            else
+                datas = redis.call('zrangebyscore', key, min_score, max_score, 'withscores')
+            end
+
+            -- set score
+            local item_list = {}
+            for i=1, #datas, 2 do
+                table.insert(item_list, datas[i])
+                redis.call('zadd', key, set_score, datas[i])
+            end
+            return item_list
+        """)
+
+        self._zset_increase_score_script = self.register_script("""
+            local key = KEYS[1]
+            local min_score = ARGV[1]
+            local max_score = ARGV[2]
+            local increase = ARGV[3]
+            local num = ARGV[4]
+
+            -- get zset data
+            local datas = nil
+            if num ~= '' then
+                datas = redis.call('zrangebyscore', key, min_score, max_score, 'withscores', 'limit', 0, num)
+            else
+                datas = redis.call('zrangebyscore', key, min_score, max_score, 'withscores')
+            end
+
+            -- set score
+            local item_list = {}
+            for i=1, #datas, 2 do
+                table.insert(item_list, datas[i])
+                redis.call('zincrby', key, increase, datas[i])
+            end
+            return item_list
+        """)
+
+        self._zser_del_score_script = self.register_script("""
+            local key = KEYS[1]
+            local min_score = ARGV[1]
+            local max_score = ARGV[2]
+            local num = ARGV[3]
+
+            -- get zset data
+            local datas = nil
+            if num ~= '' then
+                datas = redis.call('zrangebyscore', key, min_score, max_score, 'withscores', 'limit', 0, num)
+            else
+                datas = redis.call('zrangebyscore', key, min_score, max_score, 'withscores')
+            end
+
+            -- del data
+            local item_list = {}
+            for i=1, #datas, 2 do
+                table.insert(item_list, datas[i])
+                redis.call('zrem', key, datas[i])
+            end
+            return item_list
+        """)
+
+    async def zset_set_by_score(self, name: str, min: int, max: int, score: int, num: int = '') -> list:
+        """ 获取指定区间数据并修改分数为指定值 """
+        return await self._zset_set_score_script(keys=[name], args=[min, max, score, num])
+
+    async def zset_increase_by_score(self, name: str, min: int, max: int, increment: int, num: int = '') -> list:
+        """ 获取指定区间数据并修改分数加上增量 """
+        return await self._zset_increase_score_script(keys=[name], args=[min, max, increment, num])
+
+    async def zset_del_by_score(self, name: str, min: int, max: int, num: int = '') -> list:
+        """ 获取指定区间数据并删除 """
+        return await self._zser_del_score_script(keys=[name], args=[min, max, num])
+
+    async def zset_fetch_by_sorce(self, name: str,  min: int, max: int, num: int = None) -> list:
+        """ 获取指定区间数据 """
+        return await self.zrangebyscore(name, min, max, 0, num)
+
+    async def get_conut(self, name:str):
+        """ 获取数据量 """
+        key_type = await self.type(name)
+        if key_type == 'list':
+            return await self.llen(name)
+        elif key_type == 'set':
+            return await self.scard(name)
+        elif key_type == 'zset':
+            return await self.zcard(name)
+        elif key_type == 'string':
+            return await self.strlen(name)
+        elif key_type == 'hash':
+            return await self.hlen(name)
+        else:
+            return False
+
+    async def getter(self, name: str, page_size:int = 500):
+        """ 获取key全部数据 page_size批量获取针对list,set,zset有效"""
+        key_type = await self.type(name)
+        if key_type == 'string':
+            yield await self.get(name)
+
+        elif key_type == 'list':
+            i = 0
+            while i := i+1:
+                item_list =  await self.lrange(name, page_size * (i-1), page_size * i - 1)
+                if len(item_list) < page_size:
+                    break
+                yield item_list
+            yield item_list
+
+        elif key_type == 'set':
+            item_list = []
+            async for item in self.scan_iter(name):
+                item_list.append(item)
+                if len(item_list) == page_size:
+                    yield item_list
+                    item_list = []
+            yield item_list
+
+        elif key_type == 'zset':
+            item_list = []
+            async for item in self.zscan_iter(name):
+                item_list.append(item)
+                if len(item_list) == page_size:
+                    yield item_list
+                    item_list = []
+            yield item_list
+
+        elif key_type == 'hash':
+            item_list = await self.hgetall(name)
+            yield item_list
+
+        else:
+            yield False
