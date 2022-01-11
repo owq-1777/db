dbff7d3cacb4dcc6fe608a2ac8286d82d34bde4f
blank <8783833+blank-lin@user.noreply.gitee.com>
2022-01-11 16:14:55 +0800

    updata ..

--- redis.py
+++ redis.py
@@ -10,9 +10,9 @@
 
 from typing import Any, Dict, Union
 
-import aioredis
+import aioredis  # # pip install aioredis==2
 from aioredis.connection import ConnectionPool
-from loguru import logger  # pip install aioredis==2
+from loguru import logger
 
 
 def get_asyn_redis(host: str = '127.0.0.1', port: int = 6379, password: str = None, db: Union[str, int] = 0, max_connections: int = 32, **kwargs):
@@ -32,13 +32,13 @@ def get_asyn_redis(host: str = '127.0.0.1', port: int = 6379, password: str = No
 class AsyncRedisDB(aioredis.Redis):
 
     def __init__(self, *, host: str = "localhost", port: int = 6379, db: Union[str, int] = 0, password: str = None, socket_timeout: float = None, socket_connect_timeout: float = None, socket_keepalive: bool = None, socket_keepalive_options: Dict[str, Any] = None, connection_pool: ConnectionPool = None, unix_socket_path: str = None, encoding: str = "utf-8", encoding_errors: str = "strict", decode_responses: bool = False, retry_on_timeout: bool = False, ssl: bool = False, ssl_keyfile: str = None, ssl_certfile: str = None, ssl_cert_reqs: str = "required", ssl_ca_certs: str = None, ssl_check_hostname: bool = False, max_connections: int = None, single_connection_client: bool = False, health_check_interval: int = 0, client_name: str = None, username: str = None):
+        """ 继承 aioredis.Redis 的封装类"""
         super().__init__(host=host, port=port, db=db, password=password, socket_timeout=socket_timeout, socket_connect_timeout=socket_connect_timeout, socket_keepalive=socket_keepalive, socket_keepalive_options=socket_keepalive_options, connection_pool=connection_pool, unix_socket_path=unix_socket_path, encoding=encoding, encoding_errors=encoding_errors, decode_responses=decode_responses, retry_on_timeout=retry_on_timeout, ssl=ssl, ssl_keyfile=ssl_keyfile, ssl_certfile=ssl_certfile, ssl_cert_reqs=ssl_cert_reqs, ssl_ca_certs=ssl_ca_certs, ssl_check_hostname=ssl_check_hostname, max_connections=max_connections, single_connection_client=single_connection_client, health_check_interval=health_check_interval, client_name=client_name, username=username)
 
-        self._load_lua_script()
+    async def zset_set_by_score(self, name: str, min: int, max: int, score: int, num: int = '') -> list:
+        """ 获取指定区间数据并修改分数为指定值 """
 
-    def _load_lua_script(self):
-        """ 加载 lua 脚本 """
-        self._zset_set_score_script = self.register_script("""
+        script = self.register_script("""
             local key = KEYS[1]
             local min_score = ARGV[1]
             local max_score = ARGV[2]
@@ -61,8 +61,12 @@ class AsyncRedisDB(aioredis.Redis):
             end
             return item_list
         """)
+        return await script(keys=[name], args=[min, max, score, num])
 
-        self._zset_increase_score_script = self.register_script("""
+    async def zset_increase_by_score(self, name: str, min: int, max: int, increment: int, num: int = '') -> list:
+        """ 获取指定区间数据并修改分数加上增量 """
+
+        script = self.register_script("""
             local key = KEYS[1]
             local min_score = ARGV[1]
             local max_score = ARGV[2]
@@ -85,8 +89,12 @@ class AsyncRedisDB(aioredis.Redis):
             end
             return item_list
         """)
+        return await script(keys=[name], args=[min, max, increment, num])
 
-        self._zser_del_score_script = self.register_script("""
+    async def zset_del_by_score(self, name: str, min: int, max: int, num: int = '') -> list:
+        """ 获取指定区间数据并删除 """
+
+        script = self.register_script("""
             local key = KEYS[1]
             local min_score = ARGV[1]
             local max_score = ARGV[2]
@@ -108,30 +116,20 @@ class AsyncRedisDB(aioredis.Redis):
             end
             return item_list
         """)
-
-    async def zset_set_by_score(self, name: str, min: int, max: int, score: int, num: int = '') -> list:
-        """ 获取指定区间数据并修改分数为指定值 """
-        return await self._zset_set_score_script(keys=[name], args=[min, max, score, num])
-
-    async def zset_increase_by_score(self, name: str, min: int, max: int, increment: int, num: int = '') -> list:
-        """ 获取指定区间数据并修改分数加上增量 """
-        return await self._zset_increase_score_script(keys=[name], args=[min, max, increment, num])
-
-    async def zset_del_by_score(self, name: str, min: int, max: int, num: int = '') -> list:
-        """ 获取指定区间数据并删除 """
-        return await self._zser_del_score_script(keys=[name], args=[min, max, num])
+        return await script(keys=[name], args=[min, max, num])
 
     async def zset_fetch_by_sorce(self, name: str,  min: int, max: int, num: int = None) -> list:
         """ 获取指定区间数据 """
         return await self.zrangebyscore(name, min, max, 0, num)
 
     async def _write_zset(self, name:str, data:list, score:int=0):
+        """ 批量写入zset """
         result = await self.zadd(name, dict.fromkeys(data, score))
         logger.debug(f'redis:{name} | insert {result} | updata {len(data)-result} | total {len(data)}')
         return result
 
-    async def write(self,key_type:str, name:str, data:Union[str, list], score:int=0):
-        """  """
+    async def write(self, key_type: str, name: str, data: Union[str, list], score: int = 0):
+        """ 写入操作 """
         if key_type == 'zset':
             return await self._write_zset(name, data, score)
         else:
@@ -139,6 +137,7 @@ class AsyncRedisDB(aioredis.Redis):
 
     async def get_conut(self, name:str):
         """ 获取数据量 """
+
         key_type = await self.type(name)
         if key_type == 'list':
             return await self.llen(name)
@@ -155,7 +154,9 @@ class AsyncRedisDB(aioredis.Redis):
 
     async def getter(self, name: str, page_size:int = 500):
         """ 获取key全部数据 page_size批量获取针对list,set,zset有效"""
+
         key_type = await self.type(name)
+
         if key_type == 'string':
             yield await self.get(name)
 
